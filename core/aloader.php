016c31d7e357473f641c243a3360c33d6b8f1fc9
Mikhail Chubar <m.chubar@corp.mail.ru>
2013-09-18 11:34:22 +0400

    second commit

--- core/aloader.php
+++ core/aloader.php
@@ -1,18 +1,21 @@
 <?php
 
-class aLoader {
+class aLoader
+{
 
-    function setIncludePathes(Config $config) {
-        $includePathes = $config->get('include_pathes');
-        $root = $config->get('root_folder', '');
-        foreach ($includePathes as &$path) {
-            $path = $root . $path;
-        }
-        set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $includePathes));
-    }
+	function setIncludePathes(Config $config)
+	{
+		$includePathes = $config->get('include_pathes');
+		$root = $config->get('root_folder', '');
+		foreach ($includePathes as &$path) {
+			$path = $root . $path;
+		}
+		set_include_path(get_include_path() . PATH_SEPARATOR . implode(PATH_SEPARATOR, $includePathes));
+	}
 
 }
 
-function __autoload($className) {
-    require_once($className . '.php');
+function __autoload($className)
+{
+	require_once($className . '.php');
 }
